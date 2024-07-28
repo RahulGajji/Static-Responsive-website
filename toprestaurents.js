21ebf850631926014254c311428925ae81205269
RahulGajji <rahulgajjig@gmail.com>
2024-07-28 13:35:23 +0530

    started top restcommit

---
+++ toprestaurents.js
@@ -0,0 +1,36 @@
+const toprestaurents = [
+
+
+{
+
+  restimage : 'project2-images/',
+  restname : 'La Pino\'s Pizza',
+  ratingicon : 'project2-images/icon.jpeg',
+  ratingpoints : '4.5',
+  arrivaltime : '40-45 ',
+  restvarities : 'Pizzas, Pastas, Burgers',
+  restlocation : 'Attapur' 
+
+
+},
+
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+];
